35a8145ec342a0c391017257118431cd9c9fbe77
24min <1831619583@qq.com>
2020-05-11 21:27:04 +0800

    userService😵😵😵

--- react/src/api/config.js
+++ react/src/api/config.js
@@ -2,13 +2,13 @@
  * @Author: 24min
  * @Date: 2020-05-11 20:32:41
  * @LastEditors: 24min
- * @LastEditTime: 2020-05-11 21:13:33
+ * @LastEditTime: 2020-05-11 21:20:33
  * @Description: 数据
  */
 const defaultConfig = {
     timeout: 10000, //设置超时时间
     isAutoDeletEmpty: true, //是否自动删除请求参数中的空对象 空数组 空对象 空值
-    loading: false,  //请求的接口是否启用loading
+    loading: true,  //请求的接口是否启用loading
     loadingText: "拼命加载中>_<",//自定义loading的加载文案
     import: false,  //该接口是否为上传接口 暂未用到
     export: false, //该接口是否为导出接口
--- react/src/api/service/userService.js
+++ react/src/api/service/userService.js
@@ -2,6 +2,17 @@
  * @Author: 24min
  * @Date: 2020-05-11 20:32:15
  * @LastEditors: 24min
- * @LastEditTime: 2020-05-11 20:32:33
+ * @LastEditTime: 2020-05-11 21:21:03
  * @Description: 用户模块前缀
  */
+import { callHttp } from "../request"
+import { productPrefix } from "../config"
+
+/**获取用户列表 */
+const getUserList = (params) => {
+    return callHttp(`${productPrefix.userPrefix}/userList`, 'get', params)
+}
+
+export {
+    getUserList
+}
