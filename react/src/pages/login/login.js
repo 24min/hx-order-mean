5f60397735d719181f9d6eba37aa4e58b084fdff
24min <1831619583@qq.com>
2020-05-16 08:58:17 +0800

    登录页样式  调整😁

--- react/src/pages/login/login.js
+++ react/src/pages/login/login.js
@@ -2,32 +2,35 @@
  * @Author: 24min
  * @Date: 2020-05-12 20:27:41
  * @LastEditors: 24min
- * @LastEditTime: 2020-05-14 22:35:39
+ * @LastEditTime: 2020-05-16 08:55:36
  * @Description: file content
  */
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link,useHistory } from "react-router-dom";
 import { Form, Input, Button } from 'antd'
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import './login.scss';
 
 const layout = {
-    labelCol: { span: 8 },
-    wrapperCol: { span: 16 },
+    labelCol: { span: 6 },
+    wrapperCol: { span: 12 },
 };
 const tailLayout = {
-    wrapperCol: { offset: 8, span: 16 },
+    wrapperCol: { offset: 6, span: 12 },
 };
 
-const onFinish = (value) => {
-    console.log('value', value)
-}
 /**react 函数 Login */
 function Login() {
+    let history = useHistory();
+    const onFinish = (value) => {
+        console.log('value', value)
+        history.push('/home')
+    }
     return (
         <div className="login">
             <div className="login-container">
                 <Form
+                    className="login-form"
                     {...layout}
                     name="basic"
                     initialValues={{ remember: true }}
