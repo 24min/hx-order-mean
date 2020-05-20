e75c49f0876f7b43025e3b657e714df015a4de4d
24min <1831619583@qq.com>
2020-05-20 21:59:09 +0800

    hook 状态共享问题 明天继续😔

--- client/src/components/count/count.js
+++ client/src/components/count/count.js
@@ -2,29 +2,31 @@
  * @Author: 24min
  * @Date: 2020-05-20 19:59:21
  * @LastEditors: 24min
- * @LastEditTime: 2020-05-20 20:29:29
+ * @LastEditTime: 2020-05-20 21:47:19
  * @Description: 数量组件
  */
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
 import {
     PlusCircleOutlined,
     MinusCircleOutlined,
 } from '@ant-design/icons';
 
+import { commodityReducer } from "../../reduer/reduer"
+
 function Count(props) {
-    console.log('coount',props)
-    const [num, setNum] = useState(0)
-    
-    const changeNum = (type = "add") => {
-        if (type === 'add') {
-            setNum(num + 1)
-        } else {
-            setNum(num - 1)
-        }
-    }
+    const [state, dispatch] = useReducer(commodityReducer, [{num:0}]);
+    // const [num, setNum] = useState(0)
+
+    // const changeNum = (type = "add") => {
+    //     if (type === 'add') {
+    //         setNum(num + 1)
+    //     } else {
+    //         setNum(num - 1)
+    //     }
+    // }
     return (
         <div className="count">
-            <MinusCircleOutlined onClick={() => changeNum('decrease')} />{num}<PlusCircleOutlined onClick={() => changeNum('add')} />
+            <MinusCircleOutlined onClick={() => dispatch({ type: 'decrement', info: { ...props.info } })} />{state[0].num}<PlusCircleOutlined onClick={() => dispatch({ type: 'increment', info: { ...props.info } })} />
         </div>
     )
 }
--- client/src/reduer/reduer.js
+++ client/src/reduer/reduer.js
@@ -2,6 +2,51 @@
  * @Author: 24min
  * @Date: 2020-05-20 20:34:43
  * @LastEditors: 24min
- * @LastEditTime: 2020-05-20 20:34:44
+ * @LastEditTime: 2020-05-20 21:48:16
  * @Description: file content
- */ 
+ * 
+ * state = [{
+ * name:'商品名',
+ * uid:'123',
+ * price:'222',
+ * num
+ * }]
+ * 
+ * action={
+ * type:'increment',
+ * info:{
+ *  name:'商品名',
+ * uid:'123',
+ * price:'222',
+ * num
+ * }
+ * }
+ */
+
+const commodityReducer = (state, action) => {
+    let index = state.findIndex(item => item.uid === action.info.uid)
+    switch (action.type) {
+        case 'increment':
+            console.log('sss',index)
+            if (index !== -1) {
+                state[index].num = state.commodity[index].num + 1
+            } else {
+                state.push({ ...action.info, 'num': 1 })
+            }
+            return [...state.commodity];
+        case 'decrement':
+            if (state[index].num === 1) {
+                state.splice(index, 1)
+            } else {
+                state[index].num = state.commodity[index].num - 1
+            }
+            return [...state.commodity];
+        default:
+            console.log('reducerDefault')
+            throw new Error();
+    }
+}
+
+export {
+    commodityReducer
+}
