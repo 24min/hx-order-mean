4bfe2a72a058d4146635a6d56e6afe11c02e801e
24min <1831619583@qq.com>
2020-05-25 21:20:14 +0800

    save👻

--- client/src/pages/commodityManage/commodtyManage.js
+++ client/src/pages/commodityManage/commodtyManage.js
@@ -2,12 +2,12 @@
  * @Author: 24min
  * @Date: 2020-05-19 19:02:49
  * @LastEditors: 24min
- * @LastEditTime: 2020-05-23 15:59:24
+ * @LastEditTime: 2020-05-25 21:12:16
  * @Description: 商品管理 组件
  */
 import React, { useState, useEffect } from 'react'
 import { getCommdityList } from "../../api/service/commdityService"
-import { Table, Button, Space, Tooltip } from 'antd'
+import { Table, Button, Space, Tooltip, Modal, Form, Input } from 'antd'
 import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
 import "./commodityManage.scss"
 const edit = () => {
@@ -47,9 +47,11 @@ const columns = [
     }
 ]
 function CommodityManage() {
-    const [commodityList, setList] = useState([])
-    const [num, setNum] = useState(0)
-    useEffect(() => {
+    const [form] = Form.useForm();//表单的hooks
+    const [commodityList, setList] = useState([]) //商品列表
+    const [num, setNum] = useState(0) //发起请求
+    const [isShowcreate, setModelFlag] = useState(false)//是否显示对话框
+    useEffect(() => { //副作用函数
         getCommdityList().then(res => {
             if (res.code === 200) {
                 res.data.forEach(item => item.key = item.uid)
@@ -59,10 +61,50 @@ function CommodityManage() {
             }
         })
     }, [num])
-
-
+    /** 点击确认的按钮*/
+    const handleOk = () => {
+        console.log('ok')
+        setNum(num + 1)
+        setModelFlag(false)
+    }
+    /**取消 */
+    const handleCancel = () => {
+        setModelFlag(false)
+    }
+    /**打开对话框 */
+    const open = () => {
+        setModelFlag(true)
+    }
     return (
-        <Table columns={columns} dataSource={commodityList} />
+        <>
+            <Button type="primary" onClick={open}>Primary</Button>
+            <Table columns={columns} dataSource={commodityList} />
+            <Modal
+                title="新建商品"
+                maskClosable={false}
+                visible={isShowcreate}
+                onCancel={handleCancel}
+                onOk={handleOk}
+            >
+                <Form form={form}
+                    layout="vertical"
+                    name="form_in_modal"
+                    initialValues={{}}>
+                    <Form.Item
+                        name="商品名称"
+                        label="name"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input the title of collection!',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Form>
+            </Modal>
+        </>
     )
 }
 
